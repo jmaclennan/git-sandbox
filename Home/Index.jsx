3ade60e7b473a6cd56fec99478c94fb4ab0ff10c
James Maclennan <james@neon.cc>
2020-10-01 11:10:00 -0700

    initial commit

---
+++ Home/Index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const HomePage = () => {
+    return (
+        <>
+        <h1>Transform your space</h1>
+        <p>Refresh your hotel rooms in our web portal. Oversee your projects with ease, from start to finish.</p>
+        </>
+    )
+}
